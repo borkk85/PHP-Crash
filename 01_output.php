df4007168a4eedf77e57ea2917b23f0e831e6de5
borkk85 <bor.sko.mk@gmail.com>
2022-04-14 15:27:27 +0200

    file commit

---
+++ 01_output.php
@@ -0,0 +1,30 @@
+<?php 
+
+echo 'Hello';
+echo 123;
+echo '<h1>Hello</h1>';
+
+// print 'Hello';
+
+print_r('Hello');
+print_r([1, 2, 3]);
+
+// var_dump = more info
+var_dump('Hello');
+var_dump([1, 2, 3]);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My PHP Website</title>
+</head>
+<body>
+  <h1>Hello <?php echo 'Brad'; ?></h1>
+</body>
+</html>
